3f65d37c7ebc90ae4adbfa6278ce178a60f63996
Kristina <miss.krisi@abv.bg>
2019-12-07 13:23:15 +0200

    Fixed Errors

---
+++ src/main/java/otherstufftobeoragnized/UsingSessionClient.java
@@ -0,0 +1,10 @@
+package otherstufftobeoragnized;
+
+import models.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsingSessionClient {
+    private List<Product> shoppingCart = new ArrayList<>();
+}
